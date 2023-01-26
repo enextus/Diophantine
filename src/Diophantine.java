bca1802b44c9a7b34136868ec913c676017a3231
Eduard Parkhomenko <Eduard.Parkhomenko-CIC@ibm.com>
2023-01-26 15:30:35 +0100

    2

--- src/Diophantine.java
+++ src/Diophantine.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Diophantine {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the values of a, b and c: ");
+        System.out.print("a*x + b*y + c*z = d\n");
+        System.out.print("Enter the values of a: ");
         int a = input.nextInt();
+        System.out.print("Enter the values of b: ");
         int b = input.nextInt();
+        System.out.print("Enter the values of c: ");
         int c = input.nextInt();
         System.out.print("Enter the value of d: ");
         int d = input.nextInt();
