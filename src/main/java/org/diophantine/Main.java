8d0e301336b45fcee1a85f5a045745e1bfdab646
enextus <enextus@gmail.com>
2023-02-05 01:40:58 +0100

    files

---
+++ src/main/java/org/diophantine/Main.java
@@ -0,0 +1,36 @@
+package org.diophantine;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+		Tools tm = new Tools();
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("a*x + b*y + c*z = d\n");
+
+		int a = tm.inputValue("a", input);
+		int b = tm.inputValue("b", input);
+		int c = tm.inputValue("c", input);
+
+		System.out.print("Enter the value of d: ");
+		int d = input.nextInt();
+
+		boolean solved = false;
+
+		for (int x = 0; x <= d / a; x++) {
+			for (int y = 0; y <= (d - (a * x)) / b; y++) {
+				int z = (d - (a * x) - (b * y)) / c;
+				if ((a * x + b * y + c * z) == d) {
+					System.out.println("Solution: x = " + x + ", y = " + y + ", z = " + z);
+					solved = true;
+				}
+			}
+		}
+
+		if (!solved) System.out.println("No solution found.");
+	}
+
+}
---
+++ src/test/java/org/diophantine/ToolsTests.java
@@ -0,0 +1,64 @@
+package org.diophantine;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Random;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ToolsTests {
+
+	@DisplayName("Test testException")
+	@Test
+	public void testException() {
+		Exception exception = assertThrows(ArithmeticException.class, () -> {
+			int i = 1 / 0;
+		});
+
+		String expectedMessage = "/ by zero";
+		String actualMessage = exception.getMessage();
+
+		assertTrue(actualMessage.contains(expectedMessage));
+	}
+
+	@DisplayName("Test testGenInteger")
+	@Test
+	public void testGenInteger() {
+
+		final Random RANDOM = new Random();
+		int result = RANDOM.nextInt(100) + 1;
+
+		assertTrue(result >= 1 && result <= 100);
+	}
+
+	@DisplayName("Test testInputValue_5_5")
+	@Test
+	public void testInputValue_5_5() {
+		Tools tm = new Tools();
+		String inputString = "5\n";
+		InputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
+		Scanner input = new Scanner(inputStream);
+		int result = tm.inputValue("variable", input);
+
+		assertEquals(5, result);
+	}
+
+	@DisplayName("Test testInputValue_100_100")
+	@Test
+	public void testInputValue_100_100() {
+		Tools tm = new Tools();
+		String inputString = "100\n";
+		InputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
+		Scanner input = new Scanner(inputStream);
+		int result = tm.inputValue("variable", input);
+
+		assertEquals(100, result);
+	}
+
+}
