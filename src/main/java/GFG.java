e2388f3707f72aef197d2e227d04a974cb4f852c
Your Name <you@example.com>
2023-08-23 01:24:16 +0200

    0.2.8

--- src/main/java/GFG.java
+++ src/main/java/GFG.java
@@ -1,60 +1,56 @@
-// Implementing Gauss Seidel Method in Java
+// Реализация метода Гаусса-Зейделя на Java
 
 import java.io.PrintWriter;
 import java.util.Arrays;
 
 class GFG {
-    // we set a max number of iterations to
-    // prevent an infinite loop
+    // Задаем максимальное количество итераций для предотвращения бесконечного цикла
     public static final int MAX_ITERATIONS = 100;
     private double[][] M;
 
+    // Конструктор для инициализации матрицы
     public GFG(double[][] matrix) {
         M = matrix;
     }
 
-    public void print() // printing
-    {
+    // Метод для вывода матрицы
+    public void print() {
         int n = M.length;
         for (double[] doubles : M) {
-            for (int j = 0; j < n + 1; j++)
-                System.out.print(doubles[j] + " ");
+            for (int j = 0; j < n + 1; j++) System.out.print(doubles[j] + " ");
             System.out.println();
         }
     }
 
-    // attempting to change a matrix to dominant
-    // if proved that it is not
+    // Метод для преобразования матрицы в диагонально доминирующую (если это возможно)
     public boolean transformToDominant(int r, boolean[] V, int[] R) {
         int n = M.length;
+
         if (r == M.length) {
             double[][] T = new double[n][n + 1];
-            for (int i = 0; i < R.length; i++) {
-                System.arraycopy(M[R[i]], 0, T[i], 0, n + 1);
-            }
+
+            for (int i = 0; i < R.length; i++) System.arraycopy(M[R[i]], 0, T[i], 0, n + 1);
+
             M = T;
             return true;
         }
+
         for (int i = 0; i < n; i++) {
-            if (V[i])
-                continue;
+            if (V[i]) continue;
             double sum = 0;
-            for (int j = 0; j < n; j++)
-                sum += Math.abs(M[i][j]);
+            for (int j = 0; j < n; j++) sum += Math.abs(M[i][j]);
+
             if (2 * Math.abs(M[i][r]) > sum) {
-                // diagonally dominant?
                 V[i] = true;
                 R[r] = i;
-                if (transformToDominant(r + 1, V, R))
-                    return true;
+                if (transformToDominant(r + 1, V, R)) return true;
                 V[i] = false;
             }
         }
         return false;
     }
 
-    // method to check whether matrix is
-    // diagonally dominant or not
+    // Метод для проверки, является ли матрица диагонально доминирующей
     public boolean makeDominant() {
         boolean[] visited = new boolean[M.length];
         int[] rows = new int[M.length];
@@ -62,14 +58,13 @@ class GFG {
         return transformToDominant(0, visited, rows);
     }
 
-    // method to find the solution of the matrix
-    // after all conditions are satisfied
+    // Метод для нахождения решения системы уравнений после выполнения всех условий
     public void solve() {
         int iterations = 0;
         int n = M.length;
         double epsilon = 1e-15;
-        double[] X = new double[n]; // Approximations
-        double[] P = new double[n]; // Prev
+        double[] X = new double[n]; // Текущие приближения
+        double[] P = new double[n]; // Предыдущие значения
 
         Arrays.fill(X, 0);
 
@@ -78,27 +73,25 @@ class GFG {
         for (double el : X) System.out.println("el: " + el);
 
         while (true) {
+            // Реализация итераций метода Гаусса-Зейделя
             for (int i = 0; i < n; i++) {
-                double sum = M[i][n]; // b_n
+                double sum = M[i][n];
                 for (int j = 0; j < n; j++)
                     if (j != i)
                         sum -= M[i][j] * X[j];
-                // Update xi to use in the next
-                // row calculation
                 X[i] = 1 / M[i][i] * sum;
             }
 
+            // Вывод текущего приближения
             System.out.print("X" + iterations + " = {");
-
             for (int i = 0; i < n; i++) System.out.print(X[i] + " ");
 
             System.out.println("}");
             iterations++;
 
+            // Условия остановки
             if (iterations == 1) continue;
-
             boolean stop = true;
-
             for (int i = 0; i < n && stop; i++)
                 if (Math.abs(X[i] - P[i]) > epsilon) {
                     stop = false;
@@ -107,30 +100,38 @@ class GFG {
 
             if (stop || iterations == MAX_ITERATIONS) break;
 
-            P = (double[]) X.clone();
+            // Обновление массива предыдущих значений без полного копирования
+            System.arraycopy(X, 0, P, 0, n);
         }
     }
 
+    // Точка входа в программу
     public static void main(String[] args) {
         PrintWriter writer = new PrintWriter(System.out, true);
+
+        // Создается двумерный массив M размером 2 x 3:
         int n = 2, k = 1;
         double[][] M = new double[n][n + 1];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n + 1; j++)
-                M[i][j] = k++;
-        }
+        // Здесь вы создаете матрицу M с числом строк n и числом столбцов n + 1.
+        // Значения этой матрицы устанавливаются на основе переменной k,
+        // которая начинается с 1 и увеличивается на 1 с каждым новым элементом.
+        // В итоге матрица M будет выглядеть следующим образом:
+        // 1 2 3
+        // 4 5 6
+        // Это значит, что ваша система уравнений будет такова:
 
+        // 1*X1 + 2*X2 = 3
+        // 4*X1 + 5*X2 = 6
+
+        // Инициализация матрицы
+        for (int i = 0; i < n; i++) for (int j = 0; j < n + 1; j++) M[i][j] = k++;
+
+        // Создание объекта класса GFG и решение системы уравнений
         GFG gausSeidel = new GFG(M);
 
-        if (!gausSeidel.makeDominant()) {
-
-            // if it is found that a matrix cannot be
-            // changed into diagonally dominant then we
-            // return the message to the user
-
-            writer.println("The system isn't diagonally dominant: " + "The method cannot guarantee convergence.");
-        }
+        if (!gausSeidel.makeDominant())
+            writer.println("Система не является диагонально доминирующей: метод не может гарантировать сходимость.");
 
         writer.println();
         gausSeidel.print();
--- src/main/java/Tools.java
+++ src/main/java/Tools.java
@@ -2,19 +2,22 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Tools {
+    // Метод для генерации случайного целого числа от 1 до 100
     public int genInteger() {
-        final Random RANDOM = new Random();
-        int randomInt = RANDOM.nextInt(100) + 1;
-        System.out.println("Random Integer: " + randomInt);
-        return randomInt;
+        final Random RANDOM = new Random(); // Инициализация генератора случайных чисел
+        int randomInt = RANDOM.nextInt(100) + 1; // Генерация случайного числа
+        System.out.println("Random Integer: " + randomInt); // Вывод числа на экран
+        return randomInt; // Возврат сгенерированного числа
     }
 
+    // Метод для получения целочисленного значения от пользователя с проверкой на ноль
     public int inputValue(String variable, Scanner input) {
-        int value;
-        System.out.print("Enter the values of " + variable + ": ");
+        int value; // Объявление переменной для хранения значения
+        System.out.print("Enter the values of " + variable + ": "); // Приглашение к вводу
 
-        while ((value = input.nextInt()) == 0) System.out.println(variable + " cannot be 0. Enter the valid values of " + variable + " again: ");
-        return value;
+        // Цикл для получения ненулевого значения
+        while ((value = input.nextInt()) == 0)
+            System.out.println(variable + " cannot be 0. Enter the valid values of " + variable + " again: "); // Сообщение об ошибке
+        return value; // Возврат введенного значения
     }
-
 }
