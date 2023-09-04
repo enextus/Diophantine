5d616876e84384ca87842b51d06239debc39c49b
enextus <enextus@gmail.com>
2023-09-04 11:24:00 +0200

    4.0.0

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,81 +6,57 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    public boolean containsSolution(List<int[]> solutions, int[] target) {
-        Arrays.sort(target);  // Сортируем целевой массив
+	public boolean containsSolution(List<int[]> solutions, int[] target) {
+		Arrays.sort(target);  // Сортируем целевой массив
 
-        for (int[] solution : solutions) {
-            if (solution.length == target.length) {
-                int[] sortedSolution = Arrays.copyOf(solution, solution.length);
-                Arrays.sort(sortedSolution);  // Сортируем текущий массив решений
+		for (int[] solution : solutions) {
+			if (solution.length == target.length) {
+				int[] sortedSolution = Arrays.copyOf(solution, solution.length);
+				Arrays.sort(sortedSolution);  // Сортируем текущий массив решений
 
-                if (Arrays.equals(sortedSolution, target)) {
-                    return true;  // Решение найдено
-                }
-            }
-        }
+				if (Arrays.equals(sortedSolution, target)) {
+					return true;  // Решение найдено
+				}
+			}
+		}
 
-        return false;  // Решение не найдено
-    }
+		return false;  // Решение не найдено
+	}
 
-    @Test
-    void testFindSolution() {
-        List<int[]> solutions = DiophantineSolver.findSolution(1, 1, 1, 6);
+	@Test
+	void testFindSolution() {
+		List<int[]> solutions = DiophantineSolver.findSolution(1, 1, 1, 6);
 
-        assertFalse(solutions.isEmpty());
-        assertTrue(containsSolution(solutions, new int[]{1, 2, 3}));
-        assertTrue(containsSolution(solutions, new int[]{3, 2, 1}));
-    }
+		assertFalse(solutions.isEmpty());
+		assertTrue(containsSolution(solutions, new int[] {1, 2, 3}));
+		assertTrue(containsSolution(solutions, new int[] {3, 2, 1}));
+	}
 
-    // Проверяет, что ваш решатель корректно сообщает об отсутствии решения.
-    @Test
-    void testFindSolutionNoSolution() {
-        List<int[]> solutions = DiophantineSolver.findSolution(2, 2, 2, 1);
+	// Проверяет, что ваш решатель корректно сообщает об отсутствии решения.
+	@Test
+	void testFindSolutionNoSolution() {
+		List<int[]> solutions = DiophantineSolver.findSolution(2, 2, 2, 1);
 
-        assertTrue(solutions.isEmpty()); // Ожидается отсутствие решения
-    }
+		assertTrue(solutions.isEmpty()); // Ожидается отсутствие решения
+	}
 
-    // Проверяет, что ваш решатель может обрабатывать случай, когда d=0.
-    @Test
-    void testFindSolutionZeroD() {
-        List<int[]> solutions = DiophantineSolver.findSolution(1, 1, 1, 0);
+	// Проверяет, что ваш решатель может обрабатывать случай, когда d=0.
+	@Test
+	void testFindSolutionZeroD() {
+		List<int[]> solutions = DiophantineSolver.findSolution(1, 1, 1, 0);
 
-        assertFalse(solutions.isEmpty());
-        assertTrue(containsSolution(solutions, new int[]{0, 0, 0}));
-    }
+		assertFalse(solutions.isEmpty());
+		assertTrue(containsSolution(solutions, new int[] {0, 0, 0}));
+	}
 
-    @Test
-    void testFindSolutionNegativeCoefficients() {
-        List<int[]> solutions = DiophantineSolver.findSolution(-1, -1, -1, -6);
-
-
-        for (int[] solution : solutions) {
-            System.out.printf("x1 = %d, y1 = %d, z1 = %d%n", solution[0], solution[1], solution[2]);
-        }
-
-        //assertFalse(solutions.isEmpty());
-       // assertTrue(containsSolution(solutions, new int[]{-1, -2, -3}));
-       // assertTrue(containsSolution(solutions, new int[]{-3, -2, -1}));
-    }
-
-/*    @Test
-    void testFindSolutionMixedCoefficients() {
-        List<int[]> solutions = DiophantineSolver.findSolution(-1, 1, 1, 3);
-        assertFalse(solutions.isEmpty());
-        assertTrue(containsSolution(solutions, new int[]{-3, 1, 5}));
-        assertTrue(containsSolution(solutions, new int[]{-1, 1, 3}));
-    }*/
+	@Test
+	void testFindSolutionNegativeCoefficients() {
+		List<int[]> solutions = DiophantineSolver.findSolution(-1, -1, -1, -6);
 
 
-/*
-    @Test
-    void testFindSolutionBigNumbers() {
-        List<int[]> solutions = DiophantineSolver.findSolution(1000, 1000, 1000, 3000);
-        assertFalse(solutions.isEmpty());
-        assertTrue(containsSolution(solutions, new int[]{1000, 1000, 1000}));
-    }
-*/
+		for (int[] solution : solutions) {
+			System.out.printf("x1 = %d, y1 = %d, z1 = %d%n", solution[0], solution[1], solution[2]);
+		}
+	}
 
 }
-
-
